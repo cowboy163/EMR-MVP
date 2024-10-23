18cedb89d622821dae65a4d43a3e58d5a53052e6
Frank Jia <jurong@claclaws.com>
2024-10-23 10:46:43 -0400

    auth-v5.1

--- src/app/portal/saved-doctors/page.tsx
+++ src/app/portal/saved-doctors/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { fetchCurrentUserLikeIds, fetchLikedMembers } from '@/app/actions/likeActions';
 import ListsTab from '@/components/tabs/ListsTab';
 
+export const dynamic = 'force-dynamic';
+
 export default async function SavedDoctorsPage({searchParams}: {searchParams: {type: string}}) {
     const likeIds = await fetchCurrentUserLikeIds();
     const doctors = await fetchLikedMembers(searchParams.type)
